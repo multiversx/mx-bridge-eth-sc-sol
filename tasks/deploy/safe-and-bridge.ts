27084237133801da7b766ad3ef89c6cc2f568094
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2021-11-16 23:49:23 +0200

    fix: add manual gast price

--- tasks/deploy/safe-and-bridge.ts
+++ tasks/deploy/safe-and-bridge.ts
@@ -16,15 +16,19 @@ task("Contracts", "Deploys ERC20Safe and the Bridge contract")
     console.log("Admin Public Address:", adminWallet.address);
 
     const ERC20Safe = await hre.ethers.getContractFactory("ERC20Safe");
-    const safeContract = await ERC20Safe.deploy();
+    const safeContract = await ERC20Safe.deploy({ gasPrice: ethers.utils.parseUnits("141", "gwei") });
     await safeContract.deployed();
     console.log("ERC20Safe deployed to:", safeContract.address);
 
     const Bridge = await hre.ethers.getContractFactory("Bridge");
-    const bridgeContract = await Bridge.deploy(relayerAddresses, quorum, safeContract.address);
+    const bridgeContract = await Bridge.deploy(relayerAddresses, quorum, safeContract.address, {
+      gasPrice: ethers.utils.parseUnits("141", "gwei"),
+    });
     await bridgeContract.deployed();
     console.log("Bridge deployed to:", bridgeContract.address);
-    await safeContract.setBridge(bridgeContract.address);
+    await safeContract.setBridge(bridgeContract.address, {
+      gasPrice: ethers.utils.parseUnits("141", "gwei"),
+    });
 
     const fs = require("fs");
     const filename = "setup.config.json";
