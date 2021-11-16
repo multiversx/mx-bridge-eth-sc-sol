1f82dfbef80690e2a4afc9555be25ddb3123d610
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2021-11-16 21:39:04 +0200

    fix: remove hardcoded gas limits

--- tasks/add-to-whitelist.ts
+++ tasks/add-to-whitelist.ts
@@ -6,14 +6,16 @@ task(
   "Whitelists a new address in the bridge. Requires setup.config.json to be present (created with the deploy script)",
 )
   .addParam("address", "Address of the ERC20 token to be whitelisted")
+  .addParam("min-amount", "Minimum amount needed to transfer this token to Elrond")
   .setAction(async (taskArgs, hre) => {
     const tokenAddress = taskArgs.address;
+    const amount = taskArgs.amount;
     const [adminWallet] = await hre.ethers.getSigners();
     const fs = require("fs");
     const config = JSON.parse(fs.readFileSync("setup.config.json", "utf8"));
     const safeAddress = config["erc20Safe"];
     const safeContractFactory = await hre.ethers.getContractFactory("ERC20Safe");
     const safe = safeContractFactory.attach(safeAddress).connect(adminWallet);
-    await safe.whitelistToken(tokenAddress, 0, { gasPrice: ethers.utils.parseUnits("30", "gwei") });
+    await safe.whitelistToken(tokenAddress, amount);
     console.log("Token whitelisted: ", tokenAddress);
   });
