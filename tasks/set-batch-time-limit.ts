33bf5da0f276269bf038496e0c7399b5ff5ccf86
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-02-11 15:15:44 +0200

    fix(tasks): lowercase arguments name

--- tasks/set-batch-time-limit.ts
+++ tasks/set-batch-time-limit.ts
@@ -1,17 +1,16 @@
 import { task } from "hardhat/config";
-import { ethers } from "ethers";
 
 task("set-batch-time-limit", "Sets a new batch time limit")
-  .addParam("batchTimeLimit", "new batch time limit in seconds")
+  .addParam("seconds", "new batch time limit in seconds")
   .setAction(async (taskArgs, hre) => {
     const fs = require("fs");
     const filename = "setup.config.json";
-    const batchTimeLimit = taskArgs.batchTimeLimit;
+    const seconds = taskArgs.seconds;
     let config = JSON.parse(fs.readFileSync(filename, "utf8"));
     const [adminWallet] = await hre.ethers.getSigners();
     const safeAddress = config["erc20Safe"];
     const safeContractFactory = await hre.ethers.getContractFactory("ERC20Safe");
     const safe = safeContractFactory.attach(safeAddress).connect(adminWallet);
 
-    await safe.setBatchTimeLimit(batchTimeLimit);
+    await safe.setBatchTimeLimit(seconds);
   });
