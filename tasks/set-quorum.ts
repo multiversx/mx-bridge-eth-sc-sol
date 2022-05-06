5db7719ffd465fb3d0742e3557befd9e0f9aa538
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-05-06 15:04:08 +0300

    fix(deploy): set-quorum task param fix

--- tasks/set-quorum.ts
+++ tasks/set-quorum.ts
@@ -1,15 +1,15 @@
 import { task } from "hardhat/config";
 
 task("set-quorum", "Updates the quorum on the Bridge contract")
-  .addParam("newQuorumSize", "Integer representing the quorum for a transfer to be considered valid")
+  .addParam("size", "Integer representing the quorum for a transfer to be considered valid")
   .setAction(async (taskArgs, hre) => {
-    const newQuorumSize = taskArgs.newQuorumSize;
+    const size = taskArgs.size;
     const [adminWallet] = await hre.ethers.getSigners();
     const fs = require("fs");
     const config = JSON.parse(fs.readFileSync("setup.config.json", "utf8"));
     const bridgeAddress = config["bridge"];
     const bridgeContractFactory = await hre.ethers.getContractFactory("Bridge");
     const bridge = bridgeContractFactory.attach(bridgeAddress).connect(adminWallet);
-    const result = await bridge.setQuorum(newQuorumSize);
-    console.log("Quorum updated: ", newQuorumSize);
+    const result = await bridge.setQuorum(size);
+    console.log("Quorum updated: ", size);
   });
