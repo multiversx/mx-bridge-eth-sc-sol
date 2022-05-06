7bfb577ff94084dbd3bd1ca695c2a67721eda0a6
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-05-06 17:23:17 +0300

    fix(deploy): whitelist given addres from param

--- tasks/add-to-whitelist.ts
+++ tasks/add-to-whitelist.ts
@@ -1,14 +1,13 @@
 import { task } from "hardhat/config";
 
-task(
-  "add-to-whitelist",
-  "Whitelists a new address in the bridge. Requires setup.config.json to be present (created with the deploy script)",
-)
+task("add-to-whitelist", "Whitelists a new address in the bridge.")
   .addOptionalParam("minAmount", "Minimum amount allowed to transfer this token to Elrond")
   .addOptionalParam("maxAmount", "Maximum amount allowed to transfer this token to Elrond")
+  .addOptionalParam("address", "address to be whitelisted")
   .setAction(async (taskArgs, hre) => {
     const minAmount = taskArgs.minAmount ?? 25;
     const maxAmount = taskArgs.maxAmount ?? 100;
+    const address = taskArgs.address;
     const [adminWallet] = await hre.ethers.getSigners();
     const fs = require("fs");
     const config = JSON.parse(fs.readFileSync("setup.config.json", "utf8"));
@@ -16,8 +15,6 @@ task(
     const safeContractFactory = await hre.ethers.getContractFactory("ERC20Safe");
     const safe = safeContractFactory.attach(safeAddress).connect(adminWallet);
 
-    for (let token of config["tokens"]) {
-      await safe.whitelistToken(token, minAmount, maxAmount);
-      console.log("Token whitelisted: ", token);
-    }
+    await safe.whitelistToken(address, minAmount, maxAmount);
+    console.log("Token whitelisted: ", address);
   });
