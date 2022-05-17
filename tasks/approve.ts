78f40c18d4b3a23d729147762bf45b9e86b219ea
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-05-17 16:00:53 +0300

    fix(approve): fix approve task

--- tasks/approve.ts
+++ tasks/approve.ts
@@ -1,17 +1,20 @@
 import { ethers } from "ethers";
 import { task } from "hardhat/config";
 
-task("approve", "Approve token").setAction(async (taskArgs, hre) => {
-  const fs = require("fs");
-  const filename = "setup.config.json";
-  let config = JSON.parse(fs.readFileSync(filename, "utf8"));
-  const safeAddress = config["erc20Safe"];
-
-  const signers = await hre.ethers.getSigners();
-  for (let signer of signers) {
-    for (let token of config["tokens"]) {
-      const tokenContract = (await hre.ethers.getContractFactory("GenericERC20")).attach(token).connect(signer);
+task("approve", "Approve token")
+  .addParam("address", "address to be aproved")
+  .addOptionalParam("signers", "Signers count to approve token")
+  .setAction(async (taskArgs, hre) => {
+    const fs = require("fs");
+    const filename = "setup.config.json";
+    let config = JSON.parse(fs.readFileSync(filename, "utf8"));
+    const safeAddress = config["erc20Safe"];
+    const address = taskArgs.address;
+    const signersCount = taskArgs.signers ?? 1;
+    const signers = await hre.ethers.getSigners();
+    for (let i = 0; i < signersCount; i++) {
+      const signer = signers[i];
+      const tokenContract = (await hre.ethers.getContractFactory("GenericERC20")).attach(address).connect(signer);
       await tokenContract.approve(safeAddress, "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
     }
-  }
-});
+  });
