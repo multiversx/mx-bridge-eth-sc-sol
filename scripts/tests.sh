842c5aacc359b2bedd4b247450fd7d2fdb8f15d8
Iulian Pascalau <iulian.pascalau@gmail.com>
2024-07-10 10:26:50 +0300

    - test script fix

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -3,6 +3,12 @@
 export PATH=$PATH:$GOBIN:$GOPATH/bin
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
+if test -f ../.env; then
+  echo ".env file exists."
+else
+  cp ../.env.example ../.env
+fi
+
 nvm use 20
 yarn set version 1.22.22
 yarn test
