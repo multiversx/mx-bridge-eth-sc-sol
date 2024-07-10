2cf4884acfe488e95bfe8d48fe5407b00c465666
Iulian Pascalau <iulian.pascalau@gmail.com>
2024-07-10 10:31:25 +0300

    - added prints

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,8 +4,9 @@ export PATH=$PATH:$GOBIN:$GOPATH/bin
 export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 if test -f ../.env; then
-  echo ".env file exists."
+  echo ".env file exists. Nothing to do"
 else
+  echo "copying .env file from .env.example..."
   cp ../.env.example ../.env
 fi
 
